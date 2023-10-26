ba2768729f188408b45b704b52d1ab982279e166
josuerushanika <josuerushanika@gmail.com>
2023-10-26 15:27:43 +0200

    Add style

--- site/app/layout.js
+++ site/app/layout.js
@@ -25,12 +25,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${ranga.variable} ${RobotoCondensed.variable}`}>
       <body className={`font-roboto flex justify-center items-center min-h-screen`}>
-        <div className="w-[1000px] flex-col items-center pt-4 pb-4">
+        <div className="lg-w-[1000px] flex-col items-center pt-4 pb-4">
           <div className="w-full bg-slate-100 flex flex-col items-center pb-10 pt-10">
-          <div className="text-2xl mb-5 underline decoration-3 decoration-blue-400">
+          <div className="font-ranga text-2xl lg:text-4xl mb-5 underline decoration-3 decoration-blue-400">
             Josue RUSHANIKA MUSICS
           </div>
-          <div className="grid grid-cols-4 gap-1">
+          <div className="grid grid-cols-2 lg:grid-cols-4 gap-1">
             {GRID_DATA_ITEMS.map((gridDataItem) => {
               const { id, attributes } = gridDataItem;
 
@@ -38,11 +38,12 @@ export default function RootLayout({ children }) {
                 <Link href={attributes.href}>
                   <div
                     key={id}
-                    className={`${attributes.background} w-52 h-52
+                    className={`${attributes.background} w-36 h-36 lg:w-52  lg:h-52
                    flex items-end border-black border-[2px]`}
                   >
                     <div
-                      className={`bg-slate-200/75 w-full text-center mb-2
+                      className={`font-ranga bg-slate-200/75 w-full text-center mb-2
+                      text-sm lg:text-2xl h-[18px] lg:h-[30px]
                   hover:text-white  hover:bg-slate-700/75`}
                     >
                       {attributes.text}
