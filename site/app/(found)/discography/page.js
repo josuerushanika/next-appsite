f1bc05d36b4b061b8f89cac4bf85df8f89b8be11
josuerushanika <josuerushanika@gmail.com>
2023-11-02 15:44:27 +0200

    Add some package

--- site/app/(found)/discography/page.js
+++ site/app/(found)/discography/page.js
@@ -1,11 +1,20 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import { RECORDS } from "../record/[id]/data";
 
 export default function Page() {
   return (
     <div>
-      {RECORDS.map(({ id, title, date, blurb }) => (
+      {RECORDS.map(({ id, title, date, blurb, albumImage, recordHalfImage }) => (
         <div key={id}>
           <div className="w-[330px] lg:w-[500px] bg-white p-5 lg:p-10 text-left">
+            <Link href={`/record/${id}`}
+             className='flex flex-row justify-center'
+            >
+              <Image src={albumImage} width={256} height={256}/>
+              <Image src={recordHalfImage} width={128} height={256}/>
+        
+            </Link>
             {title}
             <div className="text-justify text-md lg:text-lg">{blurb}</div>
             <div className="text-sm text-slate-600">Release date: {date}</div>
